c6b29acc3609b8eb2d487ae1d1886124c65a4797
Nerdkidchiki <chikimiracle2000@gmail.com>
2023-01-04 00:38:28 +0000

    add services section

---
+++ src/components/ServicesList.jsx
@@ -0,0 +1,15 @@
+import { BiCheck } from "react-icons/bi";
+
+const ServicesList = () => {
+  return (
+    <li>
+      <BiCheck className="service__list-icon" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum,
+        voluptatem!
+      </p>
+    </li>
+  );
+};
+
+export default ServicesList;
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import ServicesList from "../components/ServicesList";
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX DESIGN</h3>
+          </div>
+          <ul className="service__list">
+            {[1, 2, 3, 4].map((index) => {
+              return <ServicesList key={index} />;
+            })}
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>WEB DEVELOPMENT</h3>
+          </div>
+          <ul className="service__list">
+            {[1, 2, 3, 4].map((index) => {
+              return <ServicesList key={index} />;
+            })}
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>CONTENT CREATION</h3>
+          </div>
+          <ul className="service__list">
+            {[1, 2, 3, 4].map((index) => {
+              return <ServicesList key={index} />;
+            })}
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
